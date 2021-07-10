7eb333955b74cd00738eea0d2d466cf888a7504d
NoahJohnG <47332136+NoahJohnG@users.noreply.github.com>
2021-07-10 00:05:31 -0700

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Noah's Portfolio</h1>
+    <span>Active Project: This Website!</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {},
+});
+</script>
