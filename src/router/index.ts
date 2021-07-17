42654082c9871b7f2f67f1b6b1286db31e372f47
NoahJohnG <47332136+NoahJohnG@users.noreply.github.com>
2021-07-16 17:43:53 -0700

    routing for about me page

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,14 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (contact.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "contact" */ '../views/Contact.vue')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
