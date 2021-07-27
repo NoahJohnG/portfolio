10df1466f894d19834ccc537a43386f9f41d1c55
NoahJohnG <47332136+NoahJohnG@users.noreply.github.com>
2021-07-27 11:10:50 -0700

    Basic project galler

--- src/components/project-tile.vue
+++ src/components/project-tile.vue
@@ -1,29 +1,57 @@
 <template>
-  <a class="project-tile">
-    {{ name }}
-  </a>
+    <a class="project-tile" @mouseover="hover = true" @mouseleave="hover = false">
+        <div class="tile-overlay" v-if="!hover">
+            <h3>{{ summary.name }}</h3>
+        </div>
+        <div class="tile-info" v-if="hover">
+            <h3>{{ summary.name }}</h3>
+            <span>{{ summary.description }}</span>
+        </div>
+    </a>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "ProjectTile",
-  props: ["name"]
+    name: "ProjectTile",
+    props: ["summary"],
+    data() {
+        return {
+            hover: false
+        }
+    }
 });
 </script>
 
 <style lang="less" scoped>
-@import "../global.less";
+@import "@/global.less";
 
 .project-tile {
-  background-color: @overlay;
-  height: 1000px;
-  border-radius: 20px;
+    background-image: linear-gradient(0deg, darken(@tertiary-color, 20%), @tertiary-color);
+    height: 500px;
+    width: 400px;
+    border-radius: 20px;
+    display: flex;
+    margin: 2em;
+}
 
-  width: 90%;
-  @media (min-width: 1600px) {
-      max-width: @max-content-width;
-  }
+.tile-content {
+    display: flex;
+    flex-direction: column;
+    flex: 1 0;
+    justify-content: center;
+    align-items: center;
+    padding: @std-padding;
+}
+
+.tile-overlay {
+    &:extend(.tile-content);
+    background-color: fade(@primary-color, 70%);
+}
+
+.tile-info {
+    &:extend(.tile-content);
+    gap: 2em;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,59 @@
 <template>
-  <div id="home">
-    <h1>Hi, I'm Noah</h1>
-    <h3>This is my portfolio website</h3>
-    <div class="project-tiles">
-      <ProjectTile :key="n" v-for="n in 5" :name="n" /> 
+    <div id="home">
+        <h1>Hi, I'm Noah</h1>
+        <h3>This is my portfolio website</h3>
+        <div class="content">
+            <div class="project-tiles">
+                <ProjectTile :key="project.name" v-for="project in projectList" :summary="project" />
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import ProjectTile from "../components/project-tile.vue";
+import ProjectJSON from "@/data.json";
+
+import ProjectTile from "@/components/project-tile.vue";
 
 export default defineComponent({
-  name: "Home",
-  components: {
-    ProjectTile
-  },
+    name: "Home",
+    components: {
+        ProjectTile
+    },
+    data() {
+        return {
+            projectList: ProjectJSON,
+        }
+    }
 });
 </script>
 
 <style lang="less" scoped>
-@import "../global.less";
+@import "@/global.less";
 
 #home {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 
-  .project-tiles {
-    display: inherit;
-    flex-direction: inherit;
-    align-items: inherit;
-    background-color: @primary-color;
+    .content {
+        background-image: linear-gradient(0deg, darken(@primary-color, 5%), @primary-color);
+        position: absolute;
+        top: 100vh;
+        width: 100%;
 
-    position: absolute;
-    top: 100vh;
-    width: 100%;
-    gap: 6em;
-    padding: 7em 0;
-  }
+        .project-tiles {
+            max-width: @max-content-width;
+            width: 90%;
+            margin: 0 auto;
+
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-evenly;
+        }
+    }
 }
 </style>
