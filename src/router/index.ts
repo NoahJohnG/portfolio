04ebf327401ef717ebb60e49bff281f4313f3463
NoahJohnG <47332136+NoahJohnG@users.noreply.github.com>
2021-08-03 12:33:36 -0700

    Added routing scroll to top behavior

--- src/router/index.ts
+++ src/router/index.ts
@@ -28,7 +28,13 @@ const routes: Array<RouteRecordRaw> = [
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
-    routes
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        }
+        return { top: 0 };
+    }
 })
 
 export default router
