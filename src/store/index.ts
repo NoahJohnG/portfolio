1b1e72cc16c60ab977a1df1b143bccf4c136d8ba
NoahJohnG <47332136+NoahJohnG@users.noreply.github.com>
2021-08-05 16:13:39 -0700

    Updating projectInfo to include role, orgs, finished, start, end, and team

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,7 +7,7 @@ export default createStore({
             /*
             "view_name": {
                 "name": string <- REQUIRED,
-                "date": string <- REQUIRED,
+                "start": string <- REQUIRED,
                 "summary": string <- REQUIRED,
                 "keywords": string[] <- RECOMENDED,
                 "image": string <- OPTIONAL
@@ -15,51 +15,96 @@ export default createStore({
             */
             "portfolio": {
                 "name": "Portfolio",
-                "date": "June 2021",
+                "role": "Creator",
+                "orgs": ["Personal Project"],
                 "summary": "Did a thing or something.",
+                "finished": true,
+                "start": new Date("June 2021"),
                 "keywords": ["Vue", "HTML", "Less", "Typescript"],
                 "image": "/SCRiPT.png"
             },
             "chess": {
                 "name": "Physical Chess AI",
-                "date": "January 2021",
+                "role": "Creator",
+                "orgs": ["Cal Poly SLO"],
                 "summary": "Designed a chessboard that automatically detects a players move, tracks the board's state, and communicates an AI's response.",
+                "finished": false,
+                "start": new Date("January 2021"),
                 "keywords": ["Embedded Systems", "C", "Circuit Design", "Linux", "Soldering"],
+                "team": {
+                    "John Oliver": "https://www.linkedin.com/in/john-oliver-1b98581/"
+                },
                 "image": "/chessboard.png"
             },
             "lockheed": {
                 "name": "Scheduling Service",
-                "date": "September 2020",
+                "role": "Student",
+                "orgs": ["Lockheed Martin", "Cal Poly SLO"],
                 "summary": "Implemented a real-time scheduling service using Lockheed Martin’s SmartSat™ SDK.",
+                "finished": true,
+                "start": new Date("September 2020"),
+                "end": new Date("March 2021"),
                 "keywords": ["Python", "C++", "TCP/IP", "UI"],
+                "team": {
+                    "Chris Lupo": "https://www.linkedin.com/in/chrislupo/",
+                    "Jack Ribarich": "https://www.linkedin.com/in/jack-ribarich/",
+                    "Ryan Stamper": ""
+                },
                 "image": "/SCRiPT.png"
             },
             "statistics": {
                 "name": "Joint Distribution Visualizer",
-                "date": "August 2020",
+                "role": "Student",
+                "orgs": "Cal Poly SLO",
                 "summary": "Missed a statistics midterm smh",
+                "finished": true,
+                "start": new Date("August 2020"),
                 "keywords": ["python", "statistics"],
                 "image": "/SCRiPT.png"
             },
             "intel": {
                 "name": "SSD Firmware Upgrade",
-                "date": "June 2020",
+                "role": "SSD Firmware Developement Intern",
+                "orgs": "Intel",
                 "summary": "Spearheaded project to greatly increase SSD firmware efficiency by migrating to 64-bit binaries on existing hardware platform.",
+                "start": new Date("June 2020"),
+                "end": new Date("December 2020"),
                 "keywords": ["C", "C++", "ARM Assembly", "Green Hills MULTI IDE"],
+                "team": {
+                    "Miguel Parra": "https://www.linkedin.com/in/miguel-parra-a97b31203/",
+                    "Craig Valine": "https://www.linkedin.com/in/craig-valine-4485452/",
+                    "Denise Vitt": "https://www.linkedin.com/in/denise-vitt-0001ba2/"
+                },
                 "image": "/SCRiPT.png"
             },
             "tatext": {
                 "name": "TaText",
-                "date": "June 2019",
-                "summary": "Architected an SMS web-app that streamlines communication between recruiters and candidates.",
+                "role": "Full Stack Developer",
+                "orgs": "Thirdpool Recruiting",
+                "summary": "Architected an SMS web-app that streamlines communication between recruiters and candistarts.",
+                "finished": true,
+                "start": new Date("June 2019"),
+                "end": new Date("June 2020"),
                 "keywords": ["SaS", "Vue", "HTML", "CSS", "Javascript"],
+                "team": {
+                    "Ben Hess": "https://www.linkedin.com/in/bkhess/",
+                    "Logan Dziuk": "https://www.linkedin.com/in/logan-dziuk/"
+                },
                 "image": "/SCRiPT.png"
             },
             "wasted": {
                 "name": "WastEd",
-                "date": "February 2019",
-                "summary": "Architected an SMS web-app that streamlines communication between recruiters and candidates.",
+                "role": "Software Developer",
+                "orgs": "Hackathon",
+                "summary": "Architected an SMS web-app that streamlines communication between recruiters and candistarts.",
+                "finished": true,
+                "start": new Date("February 2019"),
                 "keywords": ["Python", "Computer Vision", "Hackathon"],
+                "team": {
+                    "Madeline Feigles": "https://www.linkedin.com/in/madelinefeigles/",
+                    "Cole Brinsfield": "https://www.linkedin.com/in/colebrinsfield/",
+                    "Zach Blalock": "https://www.linkedin.com/in/zach-blalock/"
+                },
                 "image": "/SCRiPT.png"
             }
         }
--- src/types.ts
+++ src/types.ts
@@ -2,8 +2,13 @@ export type ViewName = string;
 
 export interface ProjectDescription {
     name: string;
-    date: string;
-    description: string;
+    role: string;
+    orgs: string[];
+    summary: string;
+    finished: boolean;
+    start?: Date;
+    end?: Date;
     keywords?: string[];
+    team?: Record<string, string>;
     image?: string;
 }
