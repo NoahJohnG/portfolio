8b4db22a8358e4559a2aa40a422f5381d9f80e83
NoahJohnG <47332136+NoahJohnG@users.noreply.github.com>
2021-08-13 09:45:58 -0700

    Improved comments and some summaries

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,9 +7,14 @@ export default createStore({
             /*
             "view_name": {
                 "name": string <- REQUIRED,
-                "start": string <- REQUIRED,
+                "role": string <- REQUIRED,
+                "orgs": string[] <- REQUIRED,
                 "summary": string <- REQUIRED,
+                "finished": boolean <- REQUIRED,
+                "start": string <- OPTIONAL,
+                "end": string <- OPTIONAL,
                 "keywords": string[] <- RECOMENDED,
+                "team": record<string, string> <- OPTIONAL
                 "image": string <- OPTIONAL
             }
             */
@@ -17,7 +22,9 @@ export default createStore({
                 "name": "Portfolio",
                 "role": "Creator",
                 "orgs": ["Personal Project"],
-                "summary": "Did a thing or something.",
+                "summary": "Only so much information can fit on a single page Résumé, and websites like LinkedIn are far too generic.\
+                            In an effort to offer a more personal and in depth introduction to everything that makes up Noah,\
+                            I decided to build a website from the ground up!",
                 "finished": true,
                 "start": new Date("June 2021"),
                 "keywords": ["Vue", "HTML", "Less", "Typescript"],
@@ -27,7 +34,11 @@ export default createStore({
                 "name": "Physical Chess AI",
                 "role": "Creator",
                 "orgs": ["Cal Poly SLO"],
-                "summary": "Designed a chessboard that automatically detects a players move, tracks the board's state, and communicates an AI's response.",
+                "summary": "In an increasingly digital society, it can be hard to find hobbies that take you away from the screen.\
+                            The virtual world offers AI that allow users to play multiplayer games alone, internet connectivity\
+                            that enables connections with people far away, and uncountable resources for learning. The Physical\
+                            Chess AI seeks to take as many of these advantages as possible and pack them into the form factor\
+                            of a traditional chess board!",
                 "finished": false,
                 "start": new Date("January 2021"),
                 "keywords": ["Embedded Systems", "C", "Circuit Design", "Linux", "Soldering"],
@@ -40,7 +51,7 @@ export default createStore({
                 "name": "Scheduling Service",
                 "role": "Student",
                 "orgs": ["Lockheed Martin", "Cal Poly SLO"],
-                "summary": "Implemented a real-time scheduling service using Lockheed Martin’s SmartSat™ SDK.",
+                "summary": "Built a thing for Lockheed Martin",
                 "finished": true,
                 "start": new Date("September 2020"),
                 "end": new Date("March 2021"),
